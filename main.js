3966725425671308b45693a058b726f02a789c87
Taarini1 <98458225+Taarini1@users.noreply.github.com>
2023-03-15 19:12:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,33 @@ canvas= createCanvas(650,500);
 canvas.center;
 webcam=createCapture(VIDEO);
 webcam.hide();
+poseNet=ml5.poseNet(webcam,modelLoaded);
+poseNet.on("pose",gotPoses);
 
+
+}
+
+function modelLoaded(){
+    console.log("modelLoaded");
+}
+
+rightWristX=0;
+rightWristY=0;
+
+leftWristX=0;
+leftWristY=0;
+function gotPoses(results){
+
+    if(results.length>0){
+rightWristX=results[0].pose.rightWrist.x
+rightWristY=results[0].pose.rightWrist.y
+
+leftWristY=results[0].pose.leftWrist.x
+leftWristY=results[0].pose.leftWrist.y
+
+console.log("Right wrist X = "+rightWristX+"Right Wrist Y = "+rightWristY);
+console.log("Left wrist X = "+leftWristX+"Left Wrist Y = "+leftWristY);
+    }
 
 }
 
